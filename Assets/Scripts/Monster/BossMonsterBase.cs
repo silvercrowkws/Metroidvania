c58a8adbdbf4d046e2c0a798c787ebc4049998b9
silvercrowkws <silvercrowkws@naver.com>
2025-10-21 15:45:38 +0900

    BossMonsterBase 보스 몬스터 하는 중
    할 부분
    
    5. 레벨이 오르면 스탯 포인트를 줘서 스텟을 분배할 수 있도록 추가 필요하고
    - 스탯 포인트를 따로 기록해야 하는데 이러면..
    - Json 으로 기억하도록 했고, 나중에 로비씬에서 분해 추가해야 함
    
    6. 문으로 넘어가면 보스 방으로 이동하는 부분 필요하고
    - 보스 몬스터를 먼저 만들고 해야할 듯?
    
    7. 우클릭으로 아이템을 사용, 취소하는 부분이 아직 없고
    - Slot 클래스에서 OnUseButton 으로 아이템 사용하도록 만들어야
    
    8. 각 아이템의 기능들이 아직 없고
    9. 5번 같은 경우 로비 씬에서 기능하도록 로비 씬 만들어야 하고
    10. 로비 씬에서 난이도, 상점, 들고 갈 아이템, 보관 할 아이템 등 필요하고
    11. 플레이어가 죽으면 들고 간 아이템, 전리품, 돈, 기타 등등 초기화 할 필요가 있고
    12. 아직 몬스터 죽이면 경험치 안들어옴
    
    보스방으로 넘어오면 플레이어의 1번째 자식 Light 2D를 비활성화 시켜야할듯?
    
    HP 스탯을 기억하고 있으니까 그거 1개당 하트 HP 1개씩으로 할까?

---
+++ Assets/Scripts/Monster/BossMonsterBase.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BossMonsterBase : MonoBehaviour
+{
+    // 모든 보스가 공통 적으로 해야 할 일
+    // 체력
+    // 일정 시간 간격으로 공격을 할건데 패턴은 자식에서?
+}
